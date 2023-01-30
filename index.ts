33e62801a8fbbce7bc5373694badfd60e2c12099
Victor Wollyver <victorwollyver50@gmail.com>
2023-01-29 23:52:52 -0300

    cors

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ const app = express();
 
 app.use((req, res, next) => {
 	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "*");
 	//Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     app.use(cors());
