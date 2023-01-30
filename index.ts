4726dfc8a2f6d07d8d7559c87e92d1321aafe829
Victor Wollyver <victorwollyver50@gmail.com>
2023-01-29 23:49:51 -0300

    add cors

--- index.ts
+++ index.ts
@@ -10,8 +10,16 @@ const port = process.env.PORT || 3001
 
 const app = express();
 
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
 app.use(express.json());
-app.use(cors());
 app.use("/user", User);
 app.use('/products', Product)
 app.use('/payments', Payments)
